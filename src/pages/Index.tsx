d115620ca1f983de1ac25ceaf6d38735c0ead2b3
PromitSarker <promitsrkr@gmail.com>
2026-03-04 12:42:34 +0600

    navbar fix

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -2,6 +2,7 @@ import { Navigation } from "@/components/Navigation";
 import { Hero } from "@/components/Hero";
 import { Stats } from "@/components/Stats";
 import { RegistrationSection } from "@/components/RegistrationSection";
+import { WhyChooseUs } from "@/components/WhyChooseUs";
 import { HowItWorks } from "@/components/HowItWorks";
 import { Benefits } from "@/components/Benefits";
 import { Testimonials } from "@/components/Testimonials";
@@ -9,6 +10,7 @@ import { FAQ } from "@/components/FAQ";
 import { Contact } from "@/components/Contact";
 import { Footer } from "@/components/Footer";
 import { FAQHelper } from "@/components/FAQHelper";
+
 
 const Index = () => {
   return (
@@ -18,7 +20,11 @@ const Index = () => {
         <Hero />
         <Stats />
         <RegistrationSection />
+        <section id="why-choose-us">
+          <WhyChooseUs />
+        </section>
         <section id="how-it-works">
+
           <HowItWorks />
         </section>
         <section id="benefits">
